1c4dd682a3ec1050f77ca5a07e6d586de22e87e0
Kazi Jawad <Kazijawadulislam@gmail.com>
2025-09-20 00:43:26 +0600

    Add files via upload

---
+++ insertValues.sql
@@ -0,0 +1,11 @@
+INSERT INTO EmployeeSalary VALUES
+(1, 'Software Engineer', 60000),
+(2, 'Data Analyst', 50000),
+(3, 'HR Manager', 55000),
+(4, 'Project Coordinator', 48000),
+(5, 'Network Administrator', 52000),
+(6, 'Graphic Designer', 45000),
+(7, 'Marketing Executive', 47000),
+(8, 'Business Analyst', 58000),
+(9, 'Database Administrator', 62000),
+(10, 'IT Support Specialist', 40000)
