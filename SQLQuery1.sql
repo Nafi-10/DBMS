f4eb7c22948f1e153fe53eecc4d8143982a91127
Kazi Jawad <Kazijawadulislam@gmail.com>
2025-09-20 00:45:13 +0600

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,25 @@
+--CREATE TABLE EmployeeDemographics
+--(EmployeeID int,
+--FirstName varchar(50),
+--LastName varchar(50),
+--Age int,
+--Gender varchar(50)
+--)
+
+
+--CREATE TABLE EmployeeSalary
+--(EmployeeID int,
+--JobTitle varchar(50),
+--Salary int)
+
+INSERT INTO EmployeeDemographics VALUES
+(1, 'Rahim', 'Khan', 28, 'Male'),
+(2, 'Karim', 'Hossain', 32, 'Male'),
+(3, 'Ayesha', 'Begum', 25, 'Female'),
+(4, 'Nusrat', 'Jahan', 29, 'Female'),
+(5, 'Fahim', 'Ahmed', 35, 'Male'),
+(6, 'Mehedi', 'Hasan', 30, 'Male'),
+(7, 'Sumaiya', 'Akter', 27, 'Female'),
+(8, 'Tanjim', 'Alam', 31, 'Male'),
+(9, 'Mitu', 'Chowdhury', 26, 'Female'),
+(10, 'Sajid', 'Rahman', 34, 'Male')
